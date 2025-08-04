a992070d2d6b9bd67aa69b6573a7f2741a2be8e5
Ali Bassam <alibassam063@gmail.com>
2025-08-04 12:17:11 +0500

    token auth , fixed serializer for username and password only for now

--- JobPostingProject/core/serializers.py
+++ JobPostingProject/core/serializers.py
@@ -7,23 +7,22 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=User
-        fields=['__all__']
+        fields=['username','email','password']
         read_only_fields=['created_at','updated_at']
 
     def __str__(self):
         return f"{self.first_name} + {self.last_name}"
     
     def validate(self, data):
-        email=data["email"]
+     
+        username=data["username"]
+        if User.objects.filter(username=username).exists():            
+            raise serializers.ValidationError("username already exists")
+
+        return data
+""" email=data["email"]
 
         if User.objects.filter(email=email).exists():
             raise serializers.ValidationError("Email already exits")
 
-
-        username=data["username"]
-
-        if User.objects.filter(username=username).exists():
-            raise serializers.ValidationError("username already exists")
-
-        return data
-    
+          """
--- JobPostingProject/core/views.py
+++ JobPostingProject/core/views.py
@@ -2,37 +2,39 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from serializers import *
+from .serializers import *
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import authenticate
 # Create your views here.
 @api_view(["POST"])
 def register_user(request):
-    email= request.data["email"]
+    email= request.data["username"]
     password=request.data["password"]
     
     serializer=UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.set_password(password)
-        serializer.save()
-        Token.objects.create(User)
+        
+        user=serializer.save()
+        user.set_password(password)
+        user.save()
+        Token.objects.create(user=user)
         return Response({"status":"success","message":"user created successfully","Data":serializer.data}, status=status.HTTP_201_CREATED,)
     
     else:
-        return Response({"status":"success","message":"Invalid data","Error":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"status":"failed","message":"Invalid data","Error":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
     
 
 @api_view(["GET"])
 def login_user(request):
+    
     username=request.data["username"]
     password=request.data["password"]
-
-    user=authenticate(username,password)
+    
+    user=authenticate(username=username,password=password)
 
     if user:
-        token= Token.objects.get_or_create()
-        return Response({"status":"success","message":"login successful","Token":token},status=status.HTTP_200_OK,)
+        token , _ = Token.objects.get_or_create()
+        return Response({"status":"success","message":"login successful","Token":token.key},status=status.HTTP_200_OK,)
     else: 
         return Response ({"status":"failed","message":"Invalid data"},status=status.HTTP_400_BAD_REQUEST,)
-    
