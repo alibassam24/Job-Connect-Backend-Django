f433ef4be87313a8ef0aa2a2569e447846c5af48
Ali Bassam <145041329+alibassam24@users.noreply.github.com>
2025-08-20 10:34:58 +0500

    Update settings.py

--- JobPostingProject/JobPostingProject/settings.py
+++ JobPostingProject/JobPostingProject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-
+from decouple import config 
 # from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+&7j_kc0exs1j%5tcuu$^1ugyo8ptn90o%=cv+b3+y&!gu4ad7"
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
